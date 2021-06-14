a0f0fcceabd40d0e4e712977ae643b6a444dc028
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-06-14 23:54:46 +0700

    AppBar and Search Bar.

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant_app_flutter_ui/pages/home/components/body.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Body(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        onPressed: () {},
+        icon: SvgPicture.asset(
+          "assets/icons/menu.svg",
+        ),
+      ),
     );
   }
 }
